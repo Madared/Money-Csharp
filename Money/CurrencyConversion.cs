a6dd8ba6cf854b5c272c4e12b2e4b72efdf5808f
Dex <77392785+Madared@users.noreply.github.com>
2023-07-19 19:06:46 +0100

    New currencyConverterFactory implementation

--- Money/CurrencyConversion.cs
+++ Money/CurrencyConversion.cs
@@ -35,3 +35,41 @@ public class CurrencyConverter
                 .PipeNonNull(convertedValue => Funds.Create(convertedValue, newCurrency)));
 }
 
+public class CurrencyConverterFactory
+{
+    private Option<CurrencyConverter> _currentConverter;
+    private Option<DateTime> _dateOfConverterCreation;
+    private IConversionGetter _conversionGetter;
+
+    public CurrencyConverterFactory(IConversionGetter conversionGetter)
+    {
+        _conversionGetter= conversionGetter;
+
+        _currentConverter = Create()
+            .PipeNonNull(converter => Option<CurrencyConverter>.Some(converter));
+    }
+
+    public CurrencyConverter Get() =>
+        _currentConverter.IsNone() || ConverterExpired()
+            ? Create()
+            : _currentConverter.Data;
+
+    private CurrencyConverter Create()
+    {
+        Option<CurrencyConverter> newConverter = _conversionGetter
+            .GetConversionRates()
+                .PipeNonNull(rates => new CurrencyConverter(rates))
+                .PipeNonNull(converter => Option<CurrencyConverter>.Some(converter));
+
+        _currentConverter = newConverter;
+        _dateOfConverterCreation = Option<DateTime>.Some(DateTime.Now);
+        return _currentConverter.Data;
+    }
+
+    private bool ConverterExpired() =>
+        _dateOfConverterCreation.IsNone() ||
+        _dateOfConverterCreation.IsSome() &&
+        _dateOfConverterCreation.Data - DateTime.Now > TimeSpan.FromHours(6);
+
+}
+
